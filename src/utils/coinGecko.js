a0f46ec9fabc95a56ee545a5015a38cf8ac07457
Kevin Kuhfeldt <kevin.kuhfeldt@gmx.de>
2019-07-26 14:17:35 +0200

    create marketDataCard and connect api

--- src/utils/coinGecko.js
+++ src/utils/coinGecko.js
@@ -1,3 +1,5 @@
+import trenner from "./trenner";
+
 export function getCoinData() {
   return fetch(
     `https://api.coingecko.com/api/v3/coins/markets?vs_currency=USD&order=market_cap_desc&per_page=250&page=1&sparkline=false`
@@ -15,5 +17,26 @@ export function getCoinData() {
         };
       });
       return coinData;
+    });
+}
+
+export function getMarketData() {
+  return fetch(`https://api.coingecko.com/api/v3/global`)
+    .then(response => response.json())
+    .then(result => {
+      const marketData = {
+        total_market_cap_usd: trenner(
+          result.data.total_market_cap.usd.toFixed(0)
+        ),
+        total_volume_usd: trenner(result.data.total_volume.usd.toFixed(0)),
+        market_cap_percentage_btc: result.data.market_cap_percentage.btc.toFixed(
+          2
+        ),
+        market_cap_change_percentage_24h_usd: result.data.market_cap_change_percentage_24h_usd.toFixed(
+          2
+        )
+      };
+
+      return marketData;
     });
 }
