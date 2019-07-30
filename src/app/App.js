394286e53968dfa268975b9934a144b97d3f904f
Kevin Kuhfeldt <kevin.kuhfeldt@gmx.de>
2019-07-30 10:59:44 +0200

    add browserrouter in App.js

--- src/app/App.js
+++ src/app/App.js
@@ -1,13 +1,25 @@
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import styled from "styled-components";
 import React from "react";
 import MarketsPage from "../markets/MarketsPage";
 import GlobalStyles from "./GlobalStyle";
 
+const Grid = styled.div`
+  display: grid;
+  grid-template-rows: auto 48px;
+  height: 100vh;
+`;
+
 function App() {
   return (
-    <>
+    <Router>
       <GlobalStyles />
-      <MarketsPage />
-    </>
+      <Grid>
+        <Switch>
+          <Route path="/markets" render={props => <MarketsPage {...props} />} />
+        </Switch>
+      </Grid>
+    </Router>
   );
 }
 
