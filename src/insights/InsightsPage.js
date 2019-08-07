4e32bb51db144c38bd9b244c7321871996538477
Kevin Kuhfeldt <kevin.kuhfeldt@gmx.de>
2019-08-07 17:56:23 +0200

    create insightsPage

--- src/insights/InsightsPage.js
+++ src/insights/InsightsPage.js
@@ -1,11 +1,18 @@
 import React from "react";
 import styled from "styled-components";
 import PropTypes from "prop-types";
+import Title from "../components/Title";
+
+const StyledSection = styled.section``;
 
 function InsightsPage({ pieData }) {
   console.log(pieData);
 
-  return null;
+  return <Title size="L">Insights</Title>;
 }
+
+InsightsPage.propTypes = {
+  pieData: PropTypes.object.isRequired
+};
 
 export default InsightsPage;
