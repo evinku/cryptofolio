6ad0ca2d75785ed0e61db1cc00ed41d2c8712991
Kevin Kuhfeldt <kevin.kuhfeldt@gmx.de>
2019-08-17 17:45:31 +0200

    change email account

--- email/email.send.js
+++ email/email.send.js
@@ -1,8 +1,7 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  host: "mail.gmx.com",
-  port: 587,
+  service: "gmail",
   auth: {
     user: process.env.MAIL_USER,
     pass: process.env.MAIL_PASS
